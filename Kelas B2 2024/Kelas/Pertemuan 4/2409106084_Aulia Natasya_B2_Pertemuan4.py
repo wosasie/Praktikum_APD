74d5e313f99172ccb8f85a6a3fdbc8a6fbd43c62
wosasie <i44aul@gmail.com>
2024-09-27 15:09:13 +0800

    Pertemuan 4

---
+++ Kelas B2 2024/Kelas/Pertemuan 4/2409106084_Aulia Natasya_B2_Pertemuan4.py
@@ -0,0 +1,82 @@
+# ulang = 10
+# for i in range(ulang):
+#     # print("Perulangan ke- "+str(i))
+#     # print("Perulangan ke- "+str(i)+ " kali")
+#     print(f"Perulangan ke--{i} kali")
+
+# for i in range(1, 11):
+#     print(f"Perulangan ke--{i} kali")
+# for i in range(1, 11, 3):
+#     print(f"Perulangan ke--{i} kali")
+
+# simpan = "udin petot" [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+# print(simpan[2])
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# for i in range(1, 6, 2):
+#     for j in range(1, 6, 2):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# Buat perulangan anak ayam turun 10
+# for i in range(10, -1, -1):
+#     print(f"Anak ayam turun {i}" )
+
+# ulang = "ya"
+# hitung = 3
+# while ulang == "ya":
+#     print(f"Perulangan ke {hitung}")
+#     ulang = input("Apakah anda masih ingin mengulang? ")
+#     hitung +=1
+# print("Perulangan selesai")
+
+# x = 0 
+# while true:
+#     print X < 5!
+#     ulang = input("Apakah anda masih mengukang"? )
+#     if ulang == "tidak":
+#         break
+#     hitunh +=1
+
+# print(f"total perulangan:  {hitung}")
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     if i == 5:
+#         break
+#     print(i)
+
+# Buat program login, 
+# usn = admin
+# psw = admin123
+# salah = 0
+
+# while salah < 3:
+#     username = input("Masukkan username: ")
+#     password = input("Masukkan password: ")
+#     if usn == username and psw == passsword:
+#         print("Login berhasil ")
+#         break
+#     else:
+#         print("Login gagal")
+#         salah == 1
+
+
+
+
+
+
+
+
