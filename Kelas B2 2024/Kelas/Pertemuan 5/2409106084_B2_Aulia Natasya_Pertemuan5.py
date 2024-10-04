efd4a2fb218cb58dfa5185c705ac0dd62d932781
wosasie <i44aul@gmail.com>
2024-10-04 15:33:35 +0800

    pertemuan5

---
+++ Kelas B2 2024/Kelas/Pertemuan 5/2409106084_B2_Aulia Natasya_Pertemuan5.py
@@ -0,0 +1,88 @@
+
+# list1 = ["a",2,True]
+
+# print (type(list1))
+
+# list1 = ["a",2,True,[1,2,3]]
+
+# print (type(list1))
+
+# tas = ["buku", 32, True, 3.14, ["IF", 24]]
+# 
+# for i in tas:
+    # print (i)
+# print(len(tas))
+
+# for i in range(len(tas)):
+    # print (tas[i])
+
+# matkul = ["kalkulus", "fisdas", "pti"]
+# print (matkul)
+
+# matkul.append("Sasrea matkul")
+# print(matkul)
+
+# matsuk[1] = "Fisika Quantum"
+# print(matkul)
+
+# del matsul 1
+# print(matkul)
+
+# matkul.pop(1)
+# print(matkul)
+
+# print(f"variable buang ={buang} ")
+
+# prodi = ["Informatika", "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan", 
+# "Teknik Pertambangan", "Teknik Elektro", 
+# "Teknik Industri", "Teknik Sipil", 
+# "Teknik geologi", "Teknik kimia"]
+
+# print(prodi[::2], "\n", prodi)
+
+# pyropchar = ["hu tao", "klee", "xiangling"]
+# hydrochar = ["neuvillate", "furina", "lapu lapu"]
+
+# print(pyrochar, "\n")
+# print(hydrochar, "\n")
+# print(party)
+
+# print(pyrochar*3)
+
+# barang = [["sepatu", "kaus kaki", "baju"],
+#           ["pulpen", "pensil", "buku"]]
+# # print(barang [1][1])
+# print(barang)
+# for baris in barang:
+#     for kolom in baris:
+#         print(kolom)
+
+# perabotan = []
+# print(perabotan)
+
+# perabotan.append("meja")
+
+# print(perabotan)
+
+# nama = "aldi", "dafa", "xiyo"
+# print(nama)
+# nama.append("rava")
+# print(nama)
+
+# barang1 = ["sepatu", "tas", "buku"]
+# barang2 = ["baju", "laptop", "pensil"]
+
+# barangtotal = [["sepatu", "tas", "buku"], barang2]
+# print(barangtotal)
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# #lalu kita unpacking
+# absen, prodi, nim, nama = mahasiswa
+# #maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# #menampilkan variabel
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+
